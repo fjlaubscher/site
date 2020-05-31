72d2c0a85b4aa56a3b6175aacf44b46941a701d4
Francois Laubscher <francoislaubscher@outlook.com>
2020-05-31 17:12:01 +0200

    content updates as well as better 404 page

--- src/containers/not-found/index.tsx
+++ src/containers/not-found/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import Container from '../../components/container';
 import { ReactComponent as NotFoundSVG } from './404.svg';
 
 import styles from './styles.css';
 
 const NotFound = () => (
-  <div className={styles.notFound}>
-    <h1>Uhhh</h1>
+  <Container className={styles.container}>
+    <h2>Uhhhh</h2>
     <NotFoundSVG className={styles.image} />
     <p>This doesn&apos;t seem right...</p>
-  </div>
+  </Container>
 );
 
 export default NotFound;
